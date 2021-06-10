16eed5ff32678fe0f09ed24faaa9bf6563be0d74
Andrea <a.bizzego@gmail.com>
2021-06-10 10:59:46 +0200

    signal finalize issue

--- develop/signal_finalize.py
+++ develop/signal_finalize.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 10 08:48:04 2021
+import pyphysio as ph
+import numpy as np
 
-@author: bizzego
-"""
+#signal = ph.EvenlySignal(np.random.uniform(size=1000), 10)
 
+signal = ph.Signal(np.random.uniform(size=(1000,3,5)), 10)
+
+#%%
+from pynirs.loaders import load_nirx
+
+DATADIR = '/home/bizzego/UniTn/data/fnirs_sexism/original/2021-02-05_002'
+
+signal = load_nirx(DATADIR, False)
+
+#%%
+type(signal)
+
+type(signal+3)
+
+type(signal*3)
+
+type(signal/np.nanmean(signal))

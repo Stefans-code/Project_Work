b42d554d068d0e7bb7cbec5bf4139d85593cd8d3
Andrea Bizzego <a.bizzego@gmail.com>
2024-02-12 10:36:19 +0100

    added loaders biopac

--- pyphysio/loaders/__init__.py
+++ pyphysio/loaders/__init__.py
@@ -1,5 +1,5 @@
 from ._load_nirs import load_nirx2, load_nirx, load_snirf, load_xrnirs, SDto1darray
-from ..signal import create_signal
+from ..signal import create_signal as _create_signal
 #TODO: add modules for loading text, edf, physionet?
 
 def info_biopac(datafile):
@@ -16,5 +16,5 @@ def load_biopac(datafile, channel):
     values = channel.data
     fsamp = data.samples_per_second
     
-    signal = create_signal(values, sampling_freq=fsamp, info = {'name':channel.name})
+    signal = _create_signal(values, sampling_freq=fsamp, info = {'name':channel.name})
     return(signal)
